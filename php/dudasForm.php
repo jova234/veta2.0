a4a816a5190e829eee21b0183bd0f027eb2fa738
Alexis <jovaniidhf@gmail.com>
2023-08-01 15:11:18 -0600

    Llenado de apartado terminos y php dudas

---
+++ php/dudasForm.php
@@ -0,0 +1,42 @@
+<?php
+// Verificar si se recibió una solicitud POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obtener los datos del formulario
+    $correo = $_POST["correo"];
+    $dudas = $_POST["dudas"];
+    $comentarios = $_POST["comentarios"];
+
+    // Validar los datos recibidos (opcional, dependiendo de tus necesidades)
+
+    // Conexión a la base de datos de XAMPP
+    $servername = "localhost"; // Cambia esto si tu servidor MySQL tiene una dirección IP diferente
+    $username = "nombre_de_usuario"; // Cambia esto por el nombre de usuario de tu base de datos de XAMPP
+    $password = "contraseña"; // Cambia esto por la contraseña de tu base de datos de XAMPP
+    $dbname = "nombre_de_la_base_de_datos"; // Cambia esto por el nombre de tu base de datos de XAMPP
+
+    // Crear la conexión
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Verificar la conexión
+    if ($conn->connect_error) {
+        die("Conexión fallida: " . $conn->connect_error);
+    }
+
+    // Preparar y ejecutar la consulta para insertar los datos en la base de datos
+    $stmt = $conn->prepare("INSERT INTO tabla_de_datos (correo, dudas, comentarios) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $correo, $dudas, $comentarios);
+
+    if ($stmt->execute()) {
+        // Los datos se han insertado correctamente
+        echo "¡Gracias por enviar tus datos!";
+    } else {
+        // Ocurrió un error al insertar los datos
+        echo "Error al enviar los datos. Por favor, inténtalo de nuevo más tarde.";
+    }
+
+    // Cerrar la conexión
+    $stmt->close();
+    $conn->close();
+}
+?>
+
